8ed98f645dbec025ef02549a8e98e5a53e588e9c
m305-65\w401 <gga123@yahoo22.com.tw>
2018-04-12 17:34:41 +0800

    作業完成拉

--- rand2/Program.cs
+++ rand2/Program.cs
@@ -10,10 +10,23 @@ namespace rand2
     {
         static void Main(string[] args)
         {
+            // 1. 將所有號碼都放入 List 之中
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 42; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // 產生亂數物件
             Random rand = new Random();
             for (int i = 0; i < 6; i++)
             {
-                Console.WriteLine(rand.Next(1, 43));
+                // 2. 隨機挑選"剩下的"其中一個印出
+                int r = rand.Next(1, numbers.Count);
+                Console.Write(numbers[r]+" ");
+
+                // 3. 並從 List 中刪除
+                numbers.RemoveAt(r);
             }
 
             Console.ReadKey();
